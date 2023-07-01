02762f4891415d1499a407ff51c3c03b2581d5ef
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-07-01 22:57:52 +0300

    /

--- client/src/api/createCard.ts
+++ client/src/api/createCard.ts
@@ -1,11 +1,11 @@
-import { apiURL } from "./config";
+// import { apiURL } from "./config";
 import { TDeck } from "./getDecks";
 
 export const createCard = async (
 	deckId: string,
 	text: string
 ): Promise<TDeck> => {
-	const res = await fetch(`${apiURL}/decks/${deckId}/cards`, {
+	const res = await fetch(`/decks/${deckId}/cards`, {
 		method: "POST",
 		body: JSON.stringify({ text }),
 		headers: { "Content-Type": "application/json" },
--- client/src/api/deleteCard.ts
+++ client/src/api/deleteCard.ts
@@ -1,8 +1,8 @@
-import { apiURL } from "./config";
+// import { apiURL } from "./config";
 import { TDeck } from "./getDecks";
 
 export const deleteCard = async (id: string, index: number): Promise<TDeck> => {
-	const res = await fetch(`${apiURL}/decks/${id}/cards/${index}`, {
+	const res = await fetch(`/decks/${id}/cards/${index}`, {
 		method: "DELETE",
 	});
 	return res.json();
