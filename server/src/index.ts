9f5d648e6807705c5e06fe17fd631f41e24da98c
Mahmoud Ehab AbouDeghedy <mahmoudehab1000@gmail.com>
2023-07-02 12:24:09 +0300

    path.resolve

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,12 +11,12 @@ config();
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+app.use(express.static(path.resolve(__dirname, "..", "client", "dist")));
 
 app.use(cors({ origin: "*" }));
 app.use("/decks", deckRouter);
 app.get("*", (req, res) => {
-	res.sendFile(path.join("..", "client", "dist", "index.html"));
+	res.sendFile(path.resolve(__dirname, "..", "client", "dist", "index.html"));
 });
 
 const port = process.env.PORT || 3000;
